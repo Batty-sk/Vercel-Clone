92d7f9e7a68b3e9aac8090d6326fdf7283967ebb
Batty-sk <saorav.skumar@gmail.com>
2024-02-17 19:40:48 +0530

    added cloning logic

--- index.ts
+++ index.ts
@@ -1,22 +1,32 @@
 
 import express, { Request, Response } from 'express';
+import { Clone_Repo } from './utlis';
 
 const app = express();
 const port = 3000;
 
 app.use(express.json())
 
-app.post('/github', (req: Request, res: Response) => {
-    const { url } = req.body;
+app.get('/github', async(req: Request, res: Response) => {
+    const { url } = req.query as { url: string };
 
-    const githubRepoRegex = /^https?:\/\/github\.com\/[^\/]+\/[^\/]+$/;
-
-    if (!url || !githubRepoRegex.test(url)) {
+/*     const githubRepoRegex = /^https?:\/\/github\.com\/[^\/]+\/[^\/]+(\.git)?$/;
+ */    console.log(url)
+/*     if (!url || !githubRepoRegex.test(url)) {
         return res.status(400).send('Invalid GitHub repository URL');
+    } */
+    console.log('cloning...')
+    const cleanedUrl = url.replace(/"/g, '')
+    let result = await Clone_Repo(cleanedUrl)
+    if (result == 0){
+        res.status(500)
     }
+    else{
+        res.status(200).send('Valid GitHub repository URL');
 
-    console.log(`Valid GitHub repository URL: ${url}`);
-    res.status(200).send('Valid GitHub repository URL');
+
+    }
+    
 });
 app.listen(port, () => {
     console.log(`Server is listening at http://localhost:${port}`);
--- utlis.ts
+++ utlis.ts
@@ -1,19 +1,15 @@
-let  nodegit = require('nodegit');
+import simpleGit, { SimpleGit } from 'simple-git';
 import * as path from 'path';
 
-export const Clone_Repo=(repoLink:string)=>{
+export const Clone_Repo = async (repoLink: string): Promise<number> => {
+    const git: SimpleGit = simpleGit();
 
-    try{
-        nodegit.Clone(repoLink,__dirname+'/G-Repo/').then((val)=>{
-        console.log('successfully cloned')
-
-    }).catch((err)=>{
-        throw new Error(err)
-    })
-}
-catch(error){
-        return 0
-}
-return 1  
-
+    try {
+        await git.clone(repoLink, path.join(__dirname, 'G-Repo'));
+        console.log('Successfully cloned');
+        return 1;
+    } catch (error) {
+        console.error('Failed to clone repository:', error);
+        return 0;
+    }
 }
