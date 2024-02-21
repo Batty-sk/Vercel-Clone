8faea40fa6acfa2bbe56cee3d1a880b37bcc8961
Batty-sk <saorav.skumar@gmail.com>
2024-02-21 16:24:47 +0530

    final stage deploying service

--- uploadBucket.ts
+++ uploadBucket.ts
@@ -5,39 +5,33 @@ require('dotenv').config();
 import * as fs from 'fs';
 
 const storage = new Storage();
-
 const bucketName = 'bucket-vercel';
-
 
 import { promisify } from 'util';
 import { readdir, lstat } from 'fs';
-import { basename, join } from 'path';
+import { join } from 'path';
 
 const readdirAsync = promisify(readdir);
 const lstatAsync = promisify(lstat);
 
 export async function uploadDirectory(id: string): Promise<void> {
-    const directoryPath = `${__dirname}/G-Repo/${id}/build`;
-
-    async function uploadRecursive(filePath: string) {
-        const stats = await lstatAsync(filePath);
-
-        if (stats.isDirectory()) {
-            const files = await readdirAsync(filePath);
-            for (const file of files) {
-                await uploadRecursive(join(filePath, file));
-            }
-        } else {
-            const destination = filePath.substring(directoryPath.length + 1);
-            await storage.bucket(bucketName).upload(filePath, {
-                destination,
-            });
-            console.log(`Uploaded ${destination} to ${bucketName}.`);
-        }
-    }
-
-    await uploadRecursive(directoryPath);
+    const baseDirectory = path.resolve(__dirname, `G-Repo/${id}/build`);
+    await uploadRecursive(baseDirectory, baseDirectory,id);
     console.log('Upload complete.');
 }
 
-
+async function uploadRecursive(baseDirectory: string, directoryPath: string,id:string) {
+    const files = await readdirAsync(directoryPath);
+    for (const file of files) {
+        const filePath = join(directoryPath, file);
+        const stats = await lstatAsync(filePath);
+        if (stats.isDirectory()) {
+            await uploadRecursive(baseDirectory, filePath,id);
+        } else {
+            const relativePath = path.relative(baseDirectory, filePath);
+            const destination = `G-Repo/${id}/${relativePath}`.replace(/\\/g, '/'); // Replace backslashes with forward slashes
+            await storage.bucket(bucketName).upload(filePath, { destination });
+            console.log(`Uploaded ${filePath} to ${bucketName}/${destination}`);
+        }
+    }
+}
